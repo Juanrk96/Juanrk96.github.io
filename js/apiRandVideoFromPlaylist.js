efd9d02d7e820d8a5a99cd8fca8000a1db99eda1
Juanrk96 <rjuan5979@gmail.com>
2022-11-14 04:55:50 -0600

    Major changes to all site

---
+++ js/apiRandVideoFromPlaylist.js
@@ -0,0 +1,30 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+    var numPl = Math.floor((Math.random() * 50) + 1);
+    var player = new YT.Player("player", {
+        
+        playerVars: {
+            listType:'playlist',
+            list:'PLgPtEAlwxgkmEam7CnDEloZk0KQGQjQTU',
+            index: numPl,
+            autoplay: 1,
+},
+        events: {
+            'onReady': function (event) {
+                //event.target.cuePlaylist({list: "PLgPtEAlwxgkmEam7CnDEloZk0KQGQjQTU"});
+                //event.target.playVideo();
+                setTimeout(function() {
+                    event.target.setShuffle({'shufflePlaylist' : true});
+                }, 1000);
+            }
+        }
+    });
+}
---
+++ js/apiTheMovieDb.js
@@ -0,0 +1,28 @@
+
+const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&language=es-ES&api_key=bd551351d613eae72b5219e295facfeb&page1"
+
+fetch(API_URL)
+.then(res=>res.json())
+.then(data =>{
+	console.log(data);
+	data.results.forEach((movie) => {
+		let movieCard = `
+        <div class="col-md-4" style="padding:15px;" >
+          <div class="card h-100">
+                <img class="card-img-top" style='height: 190px; object-fit: contain' src='https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}' alt="Card image cap">
+                <div class="card-body d-flex flex-column">
+                <h2 class="card-title">${movie.original_title}</h2>
+                    <div class="mt-auto">                    
+                    
+                    <p class="card-text">${movie.overview}</p>
+                    <span class="btn btn-primary">Rates: ${movie.vote_average}</span>
+                    </div>
+                </div>
+          </div>
+        </div> 
+		`;
+		result.innerHTML += movieCard;
+ 	// console.log(movieCard);
+	
+	});
+})
